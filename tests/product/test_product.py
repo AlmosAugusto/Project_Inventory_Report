5171ea5dd4cec4a04b621ea7baa5c059d8d180f9
almosAugusto <almos.filho@hotmail.com>
2022-11-04 17:29:20 -0300

    feat: add test_cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mock = Product(
+        17,
+        'PS5',
+        'Sony',
+        '01/01/2020',
+        '01/01/2023',
+        '123456789',
+        'Frágil'
+    )
+
+    assert mock.id == 17
+    assert mock.nome_do_produto == 'PS5'
+    assert mock.nome_da_empresa == 'Sony'
+    assert mock.data_de_fabricacao == '01/01/2020'
+    assert mock.data_de_validade == '01/01/2023'
+    assert mock.numero_de_serie == '123456789'
+    assert mock.instrucoes_de_armazenamento == 'Frágil'
